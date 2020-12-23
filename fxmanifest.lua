64c42abd53b0abb51ce836b365b2e52dcfb1078c
Jeffrey Stendert <37269702+Yarpii@users.noreply.github.com>
2020-12-23 23:26:00 +0100

    .

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game  'gta5'
+
+ui_page 'ui/index.html'
+
+client_script 'client/main.js',
+
+server_script 'server/main.js',
+
+files {
+    'ui/index.html',
+    'ui/style.css',
+    'ui/app.js',
+    'ui/tablet.png',
+}
